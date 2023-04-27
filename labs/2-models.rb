d8f15d22bd998bcb017ecd5dce0973f9e09c89ed
Chris Basara <chris.basara@kellogg.northwestern.edu>
2023-04-27 02:10:16 +0000

    do

--- labs/2-models.rb
+++ labs/2-models.rb
@@ -5,7 +5,7 @@
 
 # **************************
 # DON'T CHANGE OR MOVE
-Salesperson.destroy_all
+Company.destroy_all #delete all the rows in companies
 # **************************
 
 # Lab 2: Models
@@ -19,14 +19,42 @@ Salesperson.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+puts "There are #{Salesperson.all.count} companies"
+
 # 2. insert 1-2 rows in salespeople table.
+
+new_salesperson = Salesperson.new
+
+puts new_salesperson.inspect
+
+new_salesperson["name"] = "John Smith"
+new_salesperson["title"] = "Manager"
+new_salesperson.save 
+
+puts new_salesperson.inspect
+
+salesperson2 = Salesperson.new
+salesperson2["name"] = "Karen Smith"
+salesperson2["title"] = "San Francisco"
+salesperson2.save
+
+puts "There are #{Company.all.count} companies"
 
 # 3. write code to display how many salespeople rows are in the database
 
+all_companies = Company.all
+puts all_companies.inspect
+
+cali_companies = Company.where({"state" => "CA"})
+puts cali_companies.inspect
+
+puts "CA companies: #{cali_companies.count}"
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+apple = Company.find_by({"name" => "Apple"})
+puts apple.inspect
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
